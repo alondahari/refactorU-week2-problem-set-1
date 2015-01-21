a65495ae54536e2ad5e688ba745b53b23f6719c9
Alon Dahari <climbinghobo@gmail.com>
2015-01-21 16:23:15 -0700

    add letterCapitalize

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 $(document).on('ready', function() {
-  
+	var letterCapitalize = function(str){
+		return str.replace(/(?:^| )(\w)/g, function (match) {
+			return match.toUpperCase();
+		});
+	};
+	console.log(letterCapitalize('hi whats up'));
 });
